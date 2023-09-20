36872173228c79afa235ae6b99aa4144a6a34802
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-09-20 05:08:07 +0000

    currency pairs created

--- app.rb
+++ app.rb
@@ -1,9 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "net/http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  # "
+  # <h1>Currency Pairs</h1>
+  # "
+  url = "https://api.exchangerate.host/latest"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  response_obj = JSON.parse(response)
+
+  # Extract data from the response_obj
+  @base_currency = response_obj["base"]
+  @rates = response_obj["rates"]
+  @usd_to_eur = @rates["EUR"]
+  @usd_to_gbp = @rates["GBP"]
+ 
+ @currencies = @rates.keys
+ 
+  # Render the ERB template with the data
+  erb(:pairs)
+end
+
+get("/:converting") do
+  
+end
+
+get("/:converting/:converted") do
+  erb(:converted)
 end
