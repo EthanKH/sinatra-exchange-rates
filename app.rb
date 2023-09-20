832fac5ba7504732c2eb256dd382abd594d4184c
Ethan Huang <96758695+EthanKH@users.noreply.github.com>
2023-09-20 06:12:57 +0000

    foreign exchange converter done

--- app.rb
+++ app.rb
@@ -5,30 +5,46 @@ require "net/http"
 require "json"
 
 get("/") do
-  # "
-  # <h1>Currency Pairs</h1>
-  # "
   url = "https://api.exchangerate.host/latest"
   uri = URI(url)
   response = Net::HTTP.get(uri)
   response_obj = JSON.parse(response)
 
-  # Extract data from the response_obj
   @base_currency = response_obj["base"]
   @rates = response_obj["rates"]
-  @usd_to_eur = @rates["EUR"]
-  @usd_to_gbp = @rates["GBP"]
+  @currencies = @rates.keys
  
- @currencies = @rates.keys
- 
-  # Render the ERB template with the data
   erb(:pairs)
 end
 
-get("/:converting") do
-  
+get("/:converting") do  
+  url = "https://api.exchangerate.host/latest"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  response_obj = JSON.parse(response)
+
+  @base_currency = response_obj["base"]
+  @rates = response_obj["rates"]
+  @from_currency = params[:converting]
+  @currencies = @rates.keys
+
+  erb(:converting)
 end
 
 get("/:converting/:converted") do
+  @from_currency = params[:converting]
+  @to_currency = params[:converted]
+
+  url = "https://api.exchangerate.host/convert?from=#{@from_currency}&to=#{@to_currency}"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  @conversion_data = JSON.parse(response)
+
+  @amount = @conversion_data["query"]["amount"]
+  @from_currency = @conversion_data["query"]["from"]
+  @to_currency = @conversion_data["query"]["to"]
+  @conversion_rate = @conversion_data["info"]["rate"]
+  @result = @conversion_data["result"]
+
   erb(:converted)
 end
